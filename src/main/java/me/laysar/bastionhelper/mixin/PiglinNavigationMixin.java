d2ecd1bf3c5b56964df1fa7a0c96ef0a3edc81b1
LeSaR <ilkov2006@gmail.com>
2024-11-20 14:24:21 +0300

    Fixed issue where piglin paths would show orange/red when player was in creative

--- src/main/java/me/laysar/bastionhelper/mixin/PiglinNavigationMixin.java
+++ src/main/java/me/laysar/bastionhelper/mixin/PiglinNavigationMixin.java
@@ -3,12 +3,17 @@ package me.laysar.bastionhelper.mixin;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import me.laysar.bastionhelper.handler.ShowPiglinPathsHandler;
 import me.laysar.bastionhelper.handler.ShowPiglinPathsHandler.PiglinAggroLevel;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.ai.brain.Brain;
 import net.minecraft.entity.ai.brain.MemoryModuleType;
 import net.minecraft.entity.ai.pathing.EntityNavigation;
 import net.minecraft.entity.ai.pathing.Path;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PiglinEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.GameMode;
 import org.apache.commons.lang3.mutable.MutableLong;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -19,6 +24,9 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Mixin(EntityNavigation.class)
 public abstract class PiglinNavigationMixin {
@@ -82,11 +90,21 @@ public abstract class PiglinNavigationMixin {
 
 	@Unique
 	private PiglinAggroLevel bastionhelper$aggroLevel(@NotNull PiglinEntity piglin) {
+		boolean lightAnger, mediumAnger = false, heavyAnger = false, goldDistracted;
+
 		Brain<PiglinEntity> brain = piglin.getBrain();
-		boolean lightAnger = brain.hasMemoryModule(MemoryModuleType.NEAREST_TARGETABLE_PLAYER_NOT_WEARING_GOLD);
-		boolean mediumAnger = brain.hasMemoryModule(MemoryModuleType.ANGRY_AT);
-		boolean heavyAnger = mediumAnger && brain.getOptionalMemory(MemoryModuleType.ADMIRING_DISABLED).orElse(false);
-		boolean goldDistracted = brain.hasMemoryModule(MemoryModuleType.NEAREST_VISIBLE_WANTED_ITEM);
+		lightAnger = brain.hasMemoryModule(MemoryModuleType.NEAREST_TARGETABLE_PLAYER_NOT_WEARING_GOLD);
+
+		Optional<UUID> playerUuid = brain.getOptionalMemory(MemoryModuleType.ANGRY_AT);
+		if (playerUuid.isPresent()) {
+			ServerPlayerEntity player = (ServerPlayerEntity) piglin.getEntityWorld().getPlayerByUuid(playerUuid.get());
+			if (player != null && player.interactionManager.getGameMode().isSurvivalLike()) {
+				mediumAnger = true;
+				heavyAnger = brain.getOptionalMemory(MemoryModuleType.ADMIRING_DISABLED).orElse(false);
+			}
+		}
+
+		goldDistracted = brain.hasMemoryModule(MemoryModuleType.NEAREST_VISIBLE_WANTED_ITEM);
 
 		if (heavyAnger) return PiglinAggroLevel.HEAVY;
 		if (goldDistracted) return PiglinAggroLevel.GOLD_DISTRACTED;
