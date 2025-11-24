e5cf8936bb16f1eda7e9a7f582f61dc0c752ac50
Laysar <laysar.main@gmail.com>
2025-11-23 22:00:22 -0500

    Removed unused lava deadzones

--- src/main/java/me/laysar/bastionhelper/client/render/GlobalRenderer.java
+++ src/main/java/me/laysar/bastionhelper/client/render/GlobalRenderer.java
@@ -2,7 +2,6 @@ package me.laysar.bastionhelper.client.render;
 
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
-import me.laysar.bastionhelper.client.handler.ShowLavaDeadzonesHandler;
 import me.laysar.bastionhelper.client.handler.ShowPiglinPathsHandler;
 import net.minecraft.client.util.math.MatrixStack;
 
